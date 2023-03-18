9bc8965bdadad5872d59196d43531b9a041b33e1
AlaaShalabe <93447146+AlaaShalabe@users.noreply.github.com>
2023-03-18 18:36:35 +0200

    fix some problem

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -29,14 +29,16 @@ class PostController extends Controller
     public function store(StorePostRequest $request)
     {
         $request->validated();
-        Post::create([
-            'titel' => $request->titel,
-            'category_id' => $request->category_id,
-            'content' => $request->content,
-            'imge' => $request->file('imge')->store('public/post_imges'),
-            'slug' => $request->slug = Str::lower(str_replace(' ', '-', $request->titel)),
-            'tag_id' => $request->tag_id,
-        ]);
+        $post = new Post();
+        $post->titel = $request->titel;
+        $post->category_id = $request->category_id;
+        $post->content = $request->content;
+        $post->imge = $request->file('imge')->store('public/post_imges');
+        $post->slug = $request->slug = Str::lower(str_replace(' ', '-', $request->titel));
+        $post->user_id = auth()->user();
+        $post->save();
+        $post->tags()->attach($request->tag_id);
+
 
         return redirect()->route('posts')->with('massage', 'Created Successfully');
     }
@@ -46,17 +48,11 @@ class PostController extends Controller
         return View('post.edit', ['post' => $post]);
     }
 
-    // public function update(UpdatePostRequest $request, Category $category)
-    // {
-    //     $data =  $request->validated();
-    //     $category->update($data);
-    //     return redirect()->route('categories', $category)->with('massage', 'Updated Successfully');
-    // }
 
 
     public function destory(Category $category)
     {
         $category->delete();
-        return redirect()->route('categories')->with('massage', 'Deleted Successfully');
+        return redirect()->route('posts')->with('massage', 'Deleted Successfully');
     }
 }
