8982a8fd034fec9473d9e8eb74199ce475200363
AlaaShalabe <93447146+AlaaShalabe@users.noreply.github.com>
2023-03-18 16:09:49 +0200

    layout

--- resources/views/partials/layout/navbar.blade.php
+++ resources/views/partials/layout/navbar.blade.php
@@ -29,5 +29,39 @@
                 </li>
             </ul>
         </div>
+        <div class="buttons">
+            @guest
+                @if (Route::has('login'))
+                    <a class="button is-primary" href="{{ route('login') }}">
+                        <strong>{{ __('Login') }}</strong>
+                    </a>
+                @endif
+                @if (Route::has('rigester'))
+                    <a class="button is-primary" href="{{ route('signUp') }}">
+                        <strong>{{ __('signUp') }}</strong>
+                    </a>
+                @endif
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logOut') }}"
+                            onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logOut') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+
+        </div>
     </div>
 </nav>
