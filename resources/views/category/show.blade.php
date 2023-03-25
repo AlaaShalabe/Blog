c6e6f129c4393e4a98d979702ed6beb3660e4712
AlaaShalabe <93447146+AlaaShalabe@users.noreply.github.com>
2023-03-26 00:44:01 +0200

    blade for tag and category

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -3,9 +3,52 @@
 @section('content')
     <section class="section">
         <div class="block ">
+            @if (Session::get('massage'))
+                <div class="alert alert-info mr-6 ml-6" role="alert">
+                    <li>{{ Session::get('massage') }}</li>
+                </div>
+            @endif
+        </div>
+        <div class="block ">
             <div class="titel">
                 {{ $category->name }}'s Posts
             </div>
         </div>
+        <div class="block ">
+            <div class="titel">
+                has {{ $category->posts->count() }} Posts
+            </div>
+        </div>
+        <div class="block ">
+            <div class="columns is-multiline">
+                @foreach ($category->posts as $post)
+                    <div class="column is-4">
+                        <div class="card">
+                            <div class="card-image">
+                                <figure class="image is-4by3">
+                                    <img src="{{ $post->image }} " alt="Placeholder image">
+                                </figure>
+                            </div>
+                            <div class="card-content">
+                                <div class="media">
+                                    <div class="media-content">
+                                        <p class="title is-4"> <a
+                                                href="{{ route('post.show', $post) }}">"{{ $post->titel }}"</a>
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <div class="content">
+                                    {{ $post->content }}
+                                    <br>
+                                    <time datetime="2016-1-1">{{ $post->created_at->format('d-m-y') }}</time>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+
     </section>
 @endsection
--- resources/views/tag/show.blade.php
+++ resources/views/tag/show.blade.php
@@ -1,12 +1,15 @@
 @extends('partials.app')
-@section('title', $tag->name)
+@section('title')
 @section('subtitle', "{$tag->posts->count()} posts")
 
 @section('content')
     <section class="section">
         <div class="container">
             <div class="block ">
-                <li>{{ category-> }}</li>
+                <li>{{ $tag->name }}</li>
+            </div>
+            <div class="block ">
+                <li>{{ $tag->posts->count() }} posts</li>
             </div>
             <div class="columns is-multiline">
                 @foreach ($tag->posts as $post)
@@ -14,30 +17,24 @@
                         <div class="card">
                             <div class="card-image">
                                 <figure class="image is-4by3">
-                                    <img src="{{ $post['imge'] }} " alt="Placeholder image">
+                                    <img src="{{ $post->image }} " alt="Placeholder image">
                                 </figure>
                             </div>
                             <div class="card-content">
                                 <div class="media">
-                                    <div class="media-left">
-                                        <figure class="image is-48x48">
-                                            <img src="{{ $post['auther_imge'] }} " alt="Placeholder image">
-                                        </figure>
-                                    </div>
                                     <div class="media-content">
                                         <p class="title is-4"> <a
-                                                href="{{ route('posts.show', $post) }}">{{ $post['titel'] }}</a> </p>
-                                        <p class="subtitle is-6"> {{ $post['auther'] }}</p>
+                                                href="{{ route('post.show', $post) }}">{{ $post->titel }}</a> </p>
                                     </div>
                                 </div>
 
                                 <div class="content">
-                                    {{ $post['content'] }}
+                                    {{ $post->content }}
                                     <br>
-                                    <time datetime="2016-1-1">{{ $post['created_at'] }}</time>
+                                    <time datetime="2016-1-1">{{ $post->created_at->format('d-m-y') }}</time>
                                     <br>
                                     @foreach ($post->tags as $tag)
-                                        <a href="{{ route('tags.show', $tag) }}" class="tag is-link">
+                                        <a href="{{ route('tag.show', $tag) }}" class="tag is-link">
                                             {{ $tag->name }}</a>
                                     @endforeach
                                 </div>
