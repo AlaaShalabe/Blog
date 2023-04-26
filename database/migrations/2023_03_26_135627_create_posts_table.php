8ff5115fae7826bb1eab03fd13c38fe729da524d
AlaaShalabe <93447146+AlaaShalabe@users.noreply.github.com>
2023-04-26 21:15:32 +0300

    delete posts fixed

--- database/migrations/2023_03_26_135627_create_posts_table.php
+++ database/migrations/2023_03_26_135627_create_posts_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->OnDelete('cascade');
-            $table->string('titel')->nullable();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('titel');
             $table->string('bio')->nullable();
             $table->longText('content')->nullable();
             $table->string('image')->nullable();
-            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->string('slug')->unique();
             $table->timestamps();
         });
